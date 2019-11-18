c273d69f63e2b6458b3082e93b689e5ab46ed34f
nastiacat1710@gmail.com <nastiacat1710@gmail.com>
2019-11-18 22:44:54 +0200

    zadachi JS

---
+++ Homework17/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// 1
+
+let hours = 3;
+let min = 60;
+let sec = 60;
+
+console.log( sec * min * hours );
+
+
+// 2
+
+var num = 1;
+
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num += 1;
+num -= 1;
+
+alert( num );
+
+// 3
+
+let a = 10;
+let b = 2;
+let c = 5;
+
+console.log( a + b + c );
+
+
+// 4
+
+a = 17;
+b = 10;
+c = ( a - b );
+let d = 7;
+let result = ( c + d );
+
+console.log( result );
+
+// 5
+
+let name;
+name = 'Anastasia';
+let phrase = `Привет, ${name}`;
+console.log( phrase );
